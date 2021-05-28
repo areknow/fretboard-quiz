c851df7c60c8a284b70e453704bb05f2982c1246
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-05-28 14:32:42 -0400

    basic fret board with maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { board } from "./board";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="board">
+      <table>
+        <tbody>
+          {board.map((tr, key) => (
+            <tr key={key} className={tr.invisible ? "invisible" : ""}>
+              {tr.notes.map((note, key) => {
+                if (key === 0) {
+                  return (
+                    <td key={key}>
+                      <div className="fret-label">{tr.label}</div>
+                    </td>
+                  );
+                } else {
+                  return <td key={key}></td>;
+                }
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
